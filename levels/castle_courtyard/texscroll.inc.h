78770474a24034809258a7ead79af60ea0c19a79
Shroomz64 <turtlegamer2013@gmail.com>
2022-08-23 19:00:00 -0500

    2
    2

---
+++ levels/castle_courtyard/texscroll.inc.h
@@ -0,0 +1,7 @@
+extern void scroll_castle_courtyard_dl_Circle_005_mesh_layer_5_vtx_0();
+extern void scroll_sts_mat_castle_courtyard_dl_Clouds_001();
+extern void scroll_castle_courtyard_dl_Plane_002_mesh_layer_5_vtx_0();
+extern void scroll_sts_mat_castle_courtyard_dl_water2();
+extern void scroll_castle_courtyard_dl_Plane_009_mesh_layer_5_vtx_0();
+extern void scroll_sts_mat_castle_courtyard_dl_WF_001();
+extern void scroll_castle_courtyard();
